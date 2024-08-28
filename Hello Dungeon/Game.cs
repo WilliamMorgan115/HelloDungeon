098cb68877e10339188629d71d111b1c323bb7c5
WilliamMorgan115 <wm343883@gmail.com>
2024-08-28 16:32:50 -0500

    Update Game.cs

--- Hello Dungeon/Game.cs
+++ Hello Dungeon/Game.cs
@@ -15,6 +15,7 @@ namespace Hello_Dungeon
             float playerMana = 5.0f;
             int playerGold = 3;
             float playerArmor = 5.0f;
+            string playerRole = "Warrior";
 
             Console.WriteLine("Hello, " + playerName + "!");
             Console.WriteLine();
@@ -23,9 +24,40 @@ namespace Hello_Dungeon
             Console.WriteLine("Health: " + playerHealth);
             Console.WriteLine("Mana: " + playerMana);
             Console.WriteLine("Gold: " + playerGold);
+            Console.WriteLine("Are you a Warrior or are you a Wizard?");
+            string input = "";
+            
+            while (input != "1" && input != "2")
+            {
+                Console.WriteLine("(Warrior: 1 | Wizard: 2)");
+                input = Console.ReadLine();
+                
+                if (input == "1")
+                {
+                    playerRole = "Warrior";
+                }
+                else if (input == "2")
+                {
+                    playerRole = "Wizard";
+                }
+            }
+            Console.WriteLine("Player role: " + playerRole);
             Console.WriteLine();
+            input = "";
             Console.WriteLine("You find yourself in a generic dungeon with two doors. Which door do you choose?");
-
+            while (input != "1" && input != "2")
+            {
+                Console.WriteLine("(Left: 1 | Right: 2)");
+                input = Console.ReadLine();
+                if (input == "1")
+                {
+                    Console.WriteLine("You enter the left door.");
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("You enter the right door.");
+                }
+            }
         }
     }
 }
